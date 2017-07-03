29a72c00f8ac7c3d62a5ab362deaaa45617d16cc
termie <github@anarkystic.com>
2017-07-03 13:06:24 -0700

    add wercker support
    fixes a go vet error and a test error

--- build_linux.go
+++ build_linux.go
@@ -7,8 +7,8 @@ import "syscall"
 func setAttrMappings(attr *syscall.SysProcAttr, uid int, gid int) (*syscall.SysProcAttr, error) {
 	// NOTE: requires kernel that supports userns to run without root
 	if uid != 0 {
-		attr.UidMappings = []syscall.SysProcIDMap{{0, uid, 1}}
-		attr.GidMappings = []syscall.SysProcIDMap{{0, gid, 1}}
+		attr.UidMappings = []syscall.SysProcIDMap{{ContainerID: 0, HostID: uid, Size: 1}}
+		attr.GidMappings = []syscall.SysProcIDMap{{ContainerID: 0, HostID: gid, Size: 1}}
 		attr.Cloneflags = uintptr(syscall.CLONE_NEWUSER | syscall.SIGCHLD)
 	}
 
